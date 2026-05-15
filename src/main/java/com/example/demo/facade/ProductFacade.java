a2a7ac733ba4ef6189fd31b2729b93f47db54aaf
keyloor <crkeylor23@gmail.com>
2026-05-15 09:18:51 -0600

    Component fix

--- src/main/java/com/example/demo/facade/ProductFacade.java
+++ src/main/java/com/example/demo/facade/ProductFacade.java
@@ -3,12 +3,14 @@ package com.example.demo.facade;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import com.example.demo.dtos.ProductDto;
 import com.example.demo.mappers.ProductMapper;
 import com.example.demo.services.IProductService;
 
 //La Facade coordina Services y Mappers. Oculta complejidad al Controller.
+@Component
 public class ProductFacade implements IProductFacade {
 
     @Autowired
@@ -20,7 +22,6 @@ public class ProductFacade implements IProductFacade {
     @Override
     public List<ProductDto> getAll() {
         return productMapper.toProductDtoList(productService.getAll());
-
     }
 
 }
